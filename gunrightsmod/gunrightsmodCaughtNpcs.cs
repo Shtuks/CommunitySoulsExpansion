9a420d261953e927a42cd52eae4f51d11a5e8b19
Ropro0923 <rcberry2010@gmail.com>
2025-05-18 18:32:52 -0500

    Everything was bork

--- gunrightsmod/gunrightsmodCaughtNpcs.cs
+++ gunrightsmod/gunrightsmodCaughtNpcs.cs
@@ -1,12 +1,12 @@
 using ssm.Items;
 using Terraria.ModLoader;
 using ssm.Core;
-using gunrightsmod.Content.NPCs.Politician;
+using gunrightsmod.Content.NPCs;
 
 namespace ssm.gunrightsmod
 {
-    [ExtendsFromMod(ModCompatibility.gunrightsmod.Name)]
-    [JITWhenModsEnabled(ModCompatibility.gunrightsmod.Name)]
+    [ExtendsFromMod(ModCompatibility.TerMerica.Name)]
+    [JITWhenModsEnabled(ModCompatibility.TerMerica.Name)]
     internal class gunrightsmodCaughtNpcs : ModSystem
     {
         public static void gunrightsmodRegisterItems()
--- gunrightsmod/gunrightsmodRecipes.cs
+++ gunrightsmod/gunrightsmodRecipes.cs
@@ -3,36 +3,23 @@ using Terraria.ModLoader;
 using ssm.Core;
 using Terraria.Localization;
 using FargowiltasSouls.Content.Items.Accessories.Souls;
-using ssm.gunrightsmod.Souls;
+using gunrightsmod.Content.Items;
 
 namespace ssm.gunrightsmod
 {
-    [ExtendsFromMod(ModCompatibility.gunrightsmod.Name)]
-    [JITWhenModsEnabled(ModCompatibility.gunrightsmod.Name)]
+    [ExtendsFromMod(ModCompatibility.TerMerica.Name)]
+    [JITWhenModsEnabled(ModCompatibility.TerMerica.Name)]
     public class gunrightsmodRecipes : ModSystem
     {
         public override bool IsLoadingEnabled(Mod mod)
         {
-            return ShtunConfig.Instance.gunrightsmod;
+            return ShtunConfig.Instance.TerMerica;
         }
 
         public override void AddRecipeGroups()
         {
             RecipeGroup rec = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + " Kevlar Helmet", ModContent.ItemType<KevlarBeret>(), ModContent.ItemType<KevlarFedora>(), ModContent.ItemType<KevlarHelmet>(), ModContent.ItemType<KevlarMask>(), ModContent.ItemType<KevlarVisor>());
             RecipeGroup.RegisterGroup("ssm:KevlarHelms", rec);
-        }
-
-        public override void PostAddRecipes()
-        {
-            for (int i = 0; i < Recipe.numRecipes; i++)
-            {
-                Recipe recipe = Main.recipe[i];
-
-                if (recipe.HasResult<EternitySoul>() && !recipe.HasIngredient<RevolutionSoul>())
-                {
-                    recipe.AddIngredient<RevolutionSoul>(1);
-                }
-            }
         }
     }
 }   
