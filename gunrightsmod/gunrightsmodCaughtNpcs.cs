01c952ea629290cd1cc5a17ab4f50f74f217ca0c
Ropro0923 <rcberry2010@gmail.com>
2025-05-23 17:29:57 -0500

    Create gunrightsmodCaughtNpcs.cs
    Makes it so that you can catch the politician

---
+++ gunrightsmod/gunrightsmodCaughtNpcs.cs
@@ -0,0 +1,17 @@
+using ssm.Items;
+using Terraria.ModLoader;
+using ssm.Core;
+using gunrightsmod.Content.NPCs;
+
+namespace ssm.TerMerica
+{
+    [ExtendsFromMod(ModCompatibility.TerMerica.Name)]
+    [JITWhenModsEnabled(ModCompatibility.TerMerica.Name)]
+    internal class gunrightsmodCaughtNpcs : ModSystem
+    {
+        public static void gunrightsmodRegisterItems()
+        {
+            CaughtNPCItem.Add("Politician", ModContent.NPCType<Politician>(), "'“Might be better for society to just not release this one”'");
+        }
+    }
+}
